396bda50e106a58bb01b8468f0ecb25f3f747e9b
AltF4 <altf4@phx2600.org>
2011-10-15 23:52:03 -0700

    Initial commit of hello world code. Just testing the water.

---
+++ src/ProtoType.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ProtoType.cpp
+// Author      : AltF4
+// Copyright   : GNU GPL v3
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ src/ProtoType.h
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : ProtoType.cpp
+// Author      : AltF4
+// Copyright   : GNU GPL v3
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#ifndef PROTOTYPE_H_
+#define PROTOTYPE_H_
+
+
+
+
+#endif /* PROTOTYPE_H_ */
